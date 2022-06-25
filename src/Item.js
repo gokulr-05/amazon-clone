92c1b66949276555c1e8e2ed6d4e57dcf7aaccdd
gokulr-05 <gokulravivcr05@gmail.com>
2022-06-25 14:23:46 +0530

    final commit

--- src/Item.js
+++ src/Item.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Item = ({ img, title }) => {
+const Item = ({ img, title, summary, ratings }) => {
   let [likes, setLikes] = useState(0);
   let [dislikes, setDislikes] = useState(0);
   let likeHandler = (e) => {
@@ -17,10 +17,34 @@ const Item = ({ img, title }) => {
     });
   };
   return (
-    <div className="my-5 py-5">
-      <img className="w-100 h-100" src={img} alt="vj" />
+    <div style={{ background: "black", width: "100%" }} className="my-5 py-5">
+      <div className="" style={{ width: "100%", margin: "auto" }}>
+        <img
+          className=" "
+          style={{ height: "100%", width: "100%" }}
+          src={img}
+          alt="vj"
+        />
+      </div>
       <h2 className="text-capitalize text-center">{title}</h2>
-      <div className="text-center d-flex align-items-center justify-content-center gap-5">
+      <p className="text-capitalize text-center">⭐{ratings}</p>
+      <div className="w-100">
+        <p
+          style={{
+            width: "50%",
+            margin: "auto",
+            textJustify: "inter-word",
+            textAlign: "center",
+            textTransform: "capitalize",
+          }}
+        >
+          {summary}
+        </p>
+      </div>
+      <div
+        style={{ margin: "20px" }}
+        className="text-center d-flex align-items-center justify-content-center gap-5"
+      >
         <div className="d-flex align-items-center justify-content-center gap-3">
           <button onClick={likeHandler}>👍</button>
           <p className="m-0">{likes}</p>
