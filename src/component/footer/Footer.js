536184c48b867d7e9e14eef7080f38778a2ab15c
gokulr-05 <gokulravivcr05@gmail.com>
2022-06-25 20:23:18 +0530

    final commit

---
+++ src/component/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white">
+      <p style={{ padding: "50px", textAlign: "center" }}>
+        Copyright &copy; Your Website 2022
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
