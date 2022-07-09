5d9c957228b426970bf64e5872405fa42dcce8c1
gokulr-05 <gokulravivcr05@gmail.com>
2022-07-09 15:10:37 +0530

    commit 1

--- mine/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import plSliceReducer from "./slices/plSlice";
-
-// redux store
-let store = configureStore({ reducer: { plSliceReducer: plSliceReducer } });
-
-export default store;
