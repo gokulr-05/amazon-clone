b70fbb6629734173152db4f7e32c7b4c38bebb77
gokulr-05 <gokulravivcr05@gmail.com>
2022-07-12 20:33:34 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
-import ProductDetails from "./components/productDetails/ProductDetails";
-
-import { useState } from "react";
-import ImgModal from "./components/productDetails/ImgModal/ImgModal";
 
 const App = () => {
-  let [customerModal, setCustomerModal] = useState(false);
-  let [customerImgIndex, setCustomerImgIndex] = useState(0);
+ 
 
-  let openCustomerModal = (index) => {
-    setCustomerImgIndex(index);
-    console.log("Open modal");
-    setCustomerModal(true);
-  };
-  let closeCustomerModal = () => {
-    setCustomerModal(false);
-  };
   return (
     <div>
-      <ProductDetails openCustomerModal={openCustomerModal} />
-      <ImgModal
-        customerImgIndex={customerImgIndex}
-        openCustomerModal={openCustomerModal}
-        customerModal={customerModal}
-        closeCustomerModal={closeCustomerModal}
-      />
+      <h1>app</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// bootstrap packages
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
